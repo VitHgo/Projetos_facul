19028fc7e61d8fa33e87acf28a807f8296694da9
Vitor Hugo <vhso2110trg@gmail.com>
2025-05-14 19:38:03 -0400

    Add files via upload

---
+++ AED2/lista_turtle_tkinter/questao01.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.fillcolor("blue")
+turtle.begin_fill()
+turtle.rt(120)
+turtle.forward(300)
+turtle.lt(120)
+turtle.forward(300)
+turtle.lt(120)
+turtle.forward(300)
+turtle.end_fill()
+turtle.mainloop()
+
+
