63f5e117e2da7428ddf4aa3881757d734c5ad83e
VitHgo <vhso2110trg@gmail.com>
2025-05-16 16:29:13 -0400

    Tentando fazer essa questao 8 do cão

--- AED2/lista_turtle_tkinter/questao08.py
+++ AED2/lista_turtle_tkinter/questao08.py
@@ -7,9 +7,9 @@ window.geometry("800x600")
 canvas = tk.Canvas(window, width=800, height=600, bg="white")
 canvas.pack()
 
-canvas.create_line(200,300,600,300, width=2, fill="black")
-canvas.create_line(400,500,400,100, width=2, fill="black")
+canvas.create_line(10+NumT,10,10+NumT,2*NumT+10, width=2, fill="black")
+canvas.create_line(10,10+NumT,2*NumT+10,10+NumT, width=2, fill="black")
 
-canvas.create_oval(210,310,390,490, fill='#FF6347' , outline="red")
+canvas.create_oval(10,10,(9/10*NumT),(9/10*NumT) , fill='#FF6347' , outline="red")
 
 window.mainloop()
